aa14c440a5da860ef4e0ce9233438f0aa98ebc3a
Daniel Chaves <danielchavesfa@hotmail.com>
2024-07-19 14:35:41 -0300

    feat: adds the User.js file with the model configuration.

---
+++ backend/src/models/User.js
@@ -0,0 +1,19 @@
+import { DataTypes } from 'sequelize';
+import sqlConnection from '../database/sqlConnection.js';
+import Character from './Character.js';
+
+const User = sqlConnection.define('user', {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+User.hasOne(Character);
+
+export default User;
