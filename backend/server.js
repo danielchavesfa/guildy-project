c43723431546cafc4ff349f07b3a65435b66a85c
Daniel Chaves <danielchavesfa@hotmail.com>
2024-07-19 12:17:05 -0300

    build: add cors and helmet npm dependency

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const server = express();
 const PORT = process.env.PORT || 8080;
 
+server.use(cors());
+server.use(helmet());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
