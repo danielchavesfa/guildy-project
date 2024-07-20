25155b26eb479df044356507c64eecd709c6fd29
Daniel Chaves <danielchavesfa@hotmail.com>
2024-07-20 14:38:48 -0300

    feat: adds table relationships Character with Characteristics, Invetory and Skill

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import mainRoutes from './src/routes/mainRoutes.js';
+import sqlConnection from './src/database/sqlConnection.js';
+import User from './src/models/User.js';
+import Character from './src/models/Character.js';
+import Characteristic from './src/models/Characteristic.js';
+import Inventory from './src/models/Inventory.js';
+import Skills from './src/models/Skill.js';
 
 const server = express();
 const PORT = process.env.PORT || 8080;
@@ -13,4 +19,9 @@ server.use(express.urlencoded({ extended: true }));
 
 server.use(mainRoutes);
 
-server.listen(PORT, console.log(`Server running at port: ${PORT}`));
+sqlConnection
+  .sync()
+  .then(() => {
+    server.listen(PORT, console.log(`Server running at port: ${PORT}`));
+  })
+  .catch((err) => console.error(err));
--- backend/src/models/Character.js
+++ backend/src/models/Character.js
@@ -1,6 +1,8 @@
 import { DataTypes } from 'sequelize';
 import sqlConnection from '../database/sqlConnection.js';
-import User from './User.js';
+import Characteristic from './Characteristic.js';
+import Skill from './Skill.js';
+import Inventory from './Inventory.js';
 
 const Character = sqlConnection.define('character', {
   nickname: {
@@ -32,5 +34,9 @@ const Character = sqlConnection.define('character', {
     type: DataTypes.STRING,
   },
 });
+
+Character.hasOne(Characteristic, { foreignKey: 'characterId', onDelete: 'CASCADE' });
+Character.hasOne(Skill, { foreignKey: 'characterId', onDelete: 'CASCADE' });
+Character.hasOne(Inventory, { foreignKey: 'characterId', onDelete: 'CASCADE' });
 
 export default Character;
